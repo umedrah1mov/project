7ce64d00b544c364207aedf45c2cfb4cd066d784
Umed <umedrah1mov@mail.ru>
2021-01-15 00:13:25 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+// let number = 5;
+
+// console.log(10/0); 
+
+// Взаимодействие с пользователем
+
+// const yesOrNo = confirm('Сегодня вы будете программировать?')
+// const h1El = document.querySelector('h1');
+
+// if( yesOrNo == true) {
+//     h1El.style.display = 'none';
+// } else {
+//     h1El.textContent = 'Bye World';
+// }
+
+// const textH1 = prompt('Введите текст заголовка', '');
+
+// h1El.textContent = textH1;
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?');
+// answers[1] = prompt('Как ваше фамилия?');
+// answers[2] = prompt('Сколько вам лет?');
+
+// console.log(answers);
+// document.write(answers);
+
+// Интерполяция ES6
+
+const category = 'phones';
+
+console.log(`https://someurl.com/${category}/8`);
